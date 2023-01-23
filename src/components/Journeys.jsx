2a984bb30879bb3504b5814423bbf9d1eaf68cdd
Alexandre Conte <alexandre.conte10@gmail.com>
2023-01-23 07:53:44 -0300

    Add catch error

--- src/components/Journeys.jsx
+++ src/components/Journeys.jsx
@@ -10,19 +10,21 @@ const Journeys = () => {
 
   const [journeys, setJourneys] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [hasError, setHasError] = useState(null)
 
   useEffect(() => {
     fetch('https://frontend-project.staart.com/journeys')
       .then(data => data.json())
       .then(json => setJourneys(json))
+      .catch(err => setHasError(err))
       .finally(() => setIsLoading(false))
   }, [])
 
   return (
     <JourneysSelection>
       <CenterTitle id="mainH2">Jornadas de aprendizado</CenterTitle>
-      {isLoading ?
-        <Loader />
+      {isLoading ? <Loader />
+        : hasError ? <p>Erro ao carregar os dados: {hasError.message}</p>
         : (journeys.map(journey => {
           return (
             <JourneyItem
